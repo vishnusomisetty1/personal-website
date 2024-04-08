3df21d7f6536a1fe20de0cb224ec565b1e78577b
vishnusomisetty1 <Vishnusomisetty14@gmail.com>
2024-04-07 20:23:20 -0400

    added the like component and saves

---
+++ src/app/comment/api/addLike/route.ts
@@ -0,0 +1,47 @@
+import prisma from "@/app/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+interface LikeProp {
+  id: string;
+}
+
+export async function POST(request: NextRequest) {
+  // Parsing the request body to get the comment ID
+  const body: LikeProp = await request.json();
+  const { id } = body;
+
+  // Fetching a specific comment based on the ID
+  const comment = await prisma.comment.findUnique({
+    where: {
+      id: id, // Use the ID from the request body
+    },
+  });
+
+  // Check if the comment exists before attempting to update
+  if (comment) {
+    // Updating likes for the specific comment
+    const updatedLikes = await prisma.comment.update({
+      where: {
+        id: id, // Condition to ensure only this specific comment is updated
+      },
+      data: {
+        likes: {
+          increment: 1,
+        },
+      },
+    });
+
+    console.log("Updated Comment Likes:", id, updatedLikes);
+
+    // Returning a success response
+    return new NextResponse(
+      JSON.stringify({ message: "Comment likes updated successfully" }),
+      { status: 200 },
+    );
+  } else {
+    // If the comment doesn't exist, return a 404 response
+    return new NextResponse(JSON.stringify({ error: "Comment not found" }), {
+      status: 404,
+    });
+  }
+}
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,20 +1,37 @@
-import { ThumbsUp } from "@carbon/icons-react";
+"use client";
+
+import { ThumbsUpFilled } from "@carbon/icons-react";
+import { useRouter } from "next/navigation";
 
 interface CommentProps {
   name: string;
   content: string;
   likes: number;
-  id: string; // Assuming likes is a number. Adjust if necessary
+  id: string;
 }
 
-// Use the interface with your component
 export default function Comment({ id, name, content, likes }: CommentProps) {
+  const router = useRouter();
+
+  async function handleClick() {
+    const response = await fetch("/comment/api/addLike", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    });
+    router.refresh();
+  }
+
   return (
     <div className="border p-2 text-left">
       <h3 className="text-sm text-neutral-300">{name}</h3>
       <h4>{content}</h4>
       <div className="flex flex-row items-center space-x-2">
-        <ThumbsUp />
+        <button onClick={() => handleClick()} className="hover:text-blue-500">
+          <ThumbsUpFilled />
+        </button>
         <p>{likes}</p>
       </div>
     </div>
