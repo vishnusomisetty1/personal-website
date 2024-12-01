1c655f4302c038179c0eacea527c131abe9d9ec7
vishnusomisetty1 <Vishnusomisetty14@gmail.com>
2024-11-30 21:19:21 -0500

    updated blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getBlogPosts } from "@/db/blog";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 export default function Page({ params }: { params: { slug: string } }) {
   let allBlogs = getBlogPosts();
@@ -6,13 +7,22 @@ export default function Page({ params }: { params: { slug: string } }) {
 
   if (blogPost) {
     return (
-      <div>
-        <h1>{blogPost.metadata.title}</h1>
-        <p>{blogPost.content}</p>
+      <div className="container mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
+        <article
+          className="prose prose-invert max-w-none py-12
+          [&>ol]:pl-8
+          [&>ol]:text-left
+          [&>p:not(:has(+h1)):not(:has(+h2)):not(:has(+h3))]:indent-8
+          [&>p]:text-justify
+          [&>ul]:pl-8
+          [&>ul]:text-left"
+        >
+          <h1 className="mb-8">{blogPost.metadata.title}</h1>
+          <MDXRemote source={blogPost.content} />
+        </article>
       </div>
     );
   } else {
-    // If no blog post was found, display a message
     return <div>Blog post not found.</div>;
   }
 }
